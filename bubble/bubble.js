22bd29020b3981bf12de9ad0b1749a9ad9f6b0b2
Mark Thomas <markthethomas@gmail.com>
2015-01-24 14:42:07 -0800

    Created utility generator That's about it for now

--- bubble/bubble.js
+++ bubble/bubble.js
@@ -1,36 +1,6 @@
 'use strict';
 
-function shuffle(array) {
-  var currentIndex = array.length, temporaryValue, randomIndex;
-  // While there remain elements to shuffle...
-  while (0 !== currentIndex) {
-
-    // Pick a remaining element...
-    randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex -= 1;
-
-    // And swap it with the current element.
-    temporaryValue = array[currentIndex];
-    array[currentIndex] = array[randomIndex];
-    array[randomIndex] = temporaryValue;
-  }
-  return array;
-}
-
-function generateRandomLargeArray(length) {
-  var array = new Array(length);
-  for (var i = 0; i < array.length; i++) {
-    array[i] = Math.floor(Math.random()*10);
-  }
-  shuffle(array);
-  return shuffle(array);
-}
-
-function swap(items, firstIndex, secondIndex){
-  var temp = items[firstIndex];
-  items[firstIndex] = items[secondIndex];
-  items[secondIndex] = temp;
-}
+var gen = require('../utils/generator');
 
 function bubbleSort(items){
   var len = items.length, i, j, stop;
@@ -39,12 +9,11 @@ function bubbleSort(items){
     console.log(items);
     for (j=0, stop=len-i; j < stop; j++){
       if (items[j] > items[j+1]){
-        swap(items, j, j+1);
+        gen.swap(items, j, j+1);
       }
     }
   }
   return items;
 }
 
-var array = bubbleSort(generateRandomLargeArray(5));
-console.log(array);
+console.log(bubbleSort(gen.createArray(5)));
---
+++ utils/generator.js
@@ -0,0 +1,35 @@
+'use strict';
+var gen = {
+  shuffle: function(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+
+      // And swap it with the current element.
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+    return array;
+  },
+
+  createArray: function(length) {
+    var array = new Array(length);
+    for (var i = 0; i < array.length; i++) {
+      array[i] = Math.floor(Math.random()*10);
+    }
+    gen.shuffle(array);
+    return gen.shuffle(array);
+  },
+  swap: function(items, firstIndex, secondIndex){
+    var temp = items[firstIndex];
+    items[firstIndex] = items[secondIndex];
+    items[secondIndex] = temp;
+  }
+};
+
+module.exports = gen;
